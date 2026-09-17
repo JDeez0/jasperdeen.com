a83fe1543e52964b094b1c751b7904c64688e86b
JDeez0 <jasperhdeen@gmail.com>
2026-09-17 11:55:01 -0600

    chat copy: 'Including an attempt' (not 'the attempt')

--- src/data/chat.ts
+++ src/data/chat.ts
@@ -53,6 +53,6 @@ export const dialogue: ChatMessage[] = [
       "passions) while living with my parents post-grad. Braving the " +
       "medical system for twelve months and counting, I'm grateful to be " +
       "able to focus much-needed attention on the pursuit of health. " +
-      "Including the attempt to find solutions to various digital problems.",
+      "Including an attempt to find solutions to various digital problems.",
   },
 ];
